cd637f592bf7837224f3d373b12ff34715078fbd
CsMartin <martincsihar@gmail.com>
2026-08-22 00:00:27 +0200

    Create desktop home page

--- nekotattoo/src/App.tsx
+++ nekotattoo/src/App.tsx
@@ -6,64 +6,98 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import dropdown from "../public/dropdown.png"
 import paw from "../public/pawsOutline.png"
+import Home from './Components/Home';
 const App = () => {
   const navigate = useNavigate();
-  const [gotoConsulActive, setGotoConsulActive] = useState<boolean>(false)
   const [guideWindowActive, setGuideWindowActive] = useState<boolean>(false)
-  const [gotoContactActive, setGotoContactActive] = useState<boolean>(false)
   const [aboutMeWindowActive, setAboutMeWindowActive] = useState<boolean>(false)
   const isMobile = window.innerWidth <= 768;
- 
+  const [contactActive, setContact] = useState<boolean>(false)
+  const [createConsultationActive, setCreateConsultationActive] = useState<boolean>(false)
+
+  function gotoURL(url:string){
+    navigate(url)
+    if(url == "/crucialToKnow" || url == "/faq"){
+      setGuideWindowActive(false)
+      setCreateConsultationActive(false)
+      setContact(false)
+    }
+    else if(url == "/createConsultation"){
+      setCreateConsultationActive(true)
+      setContact(false)
+      setGuideWindowActive(false)
+     setAboutMeWindowActive(false)
+    }
+    else if(url == "/contact"){
+      setContact(true)
+       setGuideWindowActive(false)
+      setAboutMeWindowActive(false)
+      setCreateConsultationActive(false)
+
+    }
+    else if(url == "/home"){
+      setGuideWindowActive(false)
+      setAboutMeWindowActive(false)
+      setCreateConsultationActive(false)
+      setContact(false)
+    }
+    else{
+      setAboutMeWindowActive(false)
+      setCreateConsultationActive(false)
+      setContact(false)
+    }
+    
+  }
 
   return (
      <>
       <div className="navbar-container">
         <div className='inner-navbar-container'>
-            <p className='navelement' onClick={()=> navigate("/createConsultation")}>Konzultáció</p>
+            <p className={createConsultationActive ? 'navelement active' : 'navelement'} onClick={()=> gotoURL("/createConsultation")}>Konzultáció</p>
             <div className="guideNavElement">
               <p className='navelement' onClick={() => setGuideWindowActive(!guideWindowActive)}>Útmutató</p>
               <img className={guideWindowActive ? 'dropdown-button dropdown-active' : "dropdown-button"} onClick={() => setGuideWindowActive(!guideWindowActive)} src={dropdown} alt="" />
               {
-                guideWindowActive ? 
-                <div className='guides-window'>
+                <div className={guideWindowActive ? 'guides-window active' : 'guides-window'} >
                   <div className="crucialToKnow">
                     <img id='paw' src={paw} alt="" />
-                    <p onClick={()=> navigate("/crucialToKnow")}>Fontos Tudnivalók</p>
+                    <p onClick={()=> gotoURL("/crucialToKnow")} >Fontos Tudnivalók</p>
                   </div>
                   <div className="faq">
                     <img id='paw' src={paw} alt="" />
-                    <p onClick={()=> navigate("/gyik")}>Gyakori kérdések</p>
+                    <p onClick={()=> gotoURL("/faq")}>Gyakran Ismételt Kérdések</p>
                   </div>
                 </div>
-                :
-                null
+                
               }
             </div>
-            <img className='navelement' src={nekoTattooLogo} onClick={()=> navigate("/home")}/>
-            <p className='navelement'onClick={()=> navigate("/contact")}>Kapcsolat</p>
+
+            <img className='navelement nekoLogo'  src={nekoTattooLogo} onClick={()=> gotoURL("/home")}/>
+
+            <p className={contactActive ? 'navelement active' : 'navelement'}onClick={()=> gotoURL("/contact")}>Kapcsolat</p>
             <div className="aboutmeNavElement">
               <p className='navelement' onClick={() => setAboutMeWindowActive(!aboutMeWindowActive)}>Rólam</p>
               <img className={aboutMeWindowActive ? 'dropdown-button dropdown-active' : "dropdown-button"} onClick={() => setAboutMeWindowActive(!aboutMeWindowActive)} src={dropdown} alt="" />
                {
-                aboutMeWindowActive && !isMobile ? 
-                <div className='aboutme-window'>
+                 
+                <div className={aboutMeWindowActive && !isMobile ? 'aboutme-window active' : 'aboutme-window'}>
                   <div className="myStory">
                     <img id='paw' src={paw} alt="" />
-                    <p onClick={()=> navigate("/myStory")}>Történetem</p>
+                    <p onClick={()=> gotoURL("/myStory")}>Történetem</p>
                   </div>
                   <div className="gallery">
                     <img id='paw' src={paw} alt="" />
-                    <p onClick={()=> navigate("/gallery")}>Galéria</p>
+                    <p onClick={()=> gotoURL("/gallery")}>Galéria</p>
                   </div>
                 </div>
-                :
-                null
+              
               }
             </div>
         </div>
       </div>
       <Routes>
         <Route path='/' element={<Navigate to="/home"/>}/>
+        <Route path='/home' element={<Home/>}/>
       </Routes>
      </>
   )
--- nekotattoo/src/Components/Home.tsx
+++ nekotattoo/src/Components/Home.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import "./Styles/Home.css"
+import homeBackground from "../../public/Photos/Kira/front.png"
+import roseBody from "../../public/roseBody.svg"
+import roseFlower from "../../public/roseLeft.svg"
+import NekoTattooText from "../../public/nekotattoo.svg"
+import paw from "../../public/pawsOutline.png"
+import { useNavigate } from 'react-router-dom'
 const Home = () => {
+  const navigate = useNavigate()
   return (
     <>
-    
+    <div className="home-main-container" data-height={window.innerHeight}>
+      <img className="roseDesignLeft" src={roseFlower} alt=""  />
+      <img className="roseDesignRight" src={roseFlower} alt=""  />
+      <div className="mid-section-container">
+        <img className='NekoTattooText' src={NekoTattooText} alt="" />
+        <p className='mottoText'>"A bőrőd a legszebb vászon, viseld rajta a történeted."</p>
+        <div className="buttons-container">
+          <button className='createConsultationButton' onClick={()=>navigate("/createConsultation")}>Konzultáció kérése <img id='paw'  src={paw} alt="" /></button>
+          <button className='myWorkButton' onClick={()=>navigate("/gallery")}>Munkáim</button>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
