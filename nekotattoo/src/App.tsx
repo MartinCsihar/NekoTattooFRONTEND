8199af614f734f97ab4fe8147f3d65b9784bbb2c
CsMartin <martincsihar@gmail.com>
2026-08-21 20:14:31 +0200

    Add assets

--- nekotattoo/src/App.tsx
+++ nekotattoo/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
-
+import "./app_style.css"
 const App = () => {
   return (
      <>
+      <div className="nav-bar-container">
+        <p id='cinzel'>CINZEL DECORATIVE</p>
+        <p id='oldtt'>OLD TT</p>
+      </div>
       <Routes>
         <Route path='/' element={<Navigate to="/home"/>}/>
       </Routes>
