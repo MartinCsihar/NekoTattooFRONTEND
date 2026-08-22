324250f4b6b8318fa10ec7a7c6c9a697673b4e24
CsMartin <martincsihar@gmail.com>
2026-08-22 15:27:50 +0200

    Add mobile NavBar

--- nekotattoo/src/Components/Navbar/MobileNavBar.tsx
+++ nekotattoo/src/Components/Navbar/MobileNavBar.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./MobileNavBar.css"
+import mobileLogo from "../../../public/mobileLogo.svg"
+import menu from "../../../public/menu.png"
+import quitFromMenu from "../../../public/close.png"
+import pawMobileNav from "../../../public/pawsOutline.png"
+import { useNavigate } from 'react-router-dom'
 const MobileNavBar = () => {
+    const navigate = useNavigate()
+    const [mobileMenuActive, setMobileMenuActive] = useState<boolean>(false)
+
+    function gotoURL(url:string){
+        navigate(url)
+        setMobileMenuActive(false)
+    }
   return (
+
    <>
     <div className="mobile-navbar-container">
-            
+        <img className='mobileLogo' src={mobileLogo} onClick={()=> gotoURL("/home")} alt="" />
+        {
+        mobileMenuActive ? 
+            <img className='quitFromMenu' onClick={()=>setMobileMenuActive(false)} src={quitFromMenu}/> 
+            :   
+            <img onClick={()=>setMobileMenuActive(!mobileMenuActive)} className='menu' src={menu} alt="" />
+        }
+        <div className={mobileMenuActive ? "mobile-dropdown active" : "mobile-dropdown"}>
+            <div className="createConsulattionMobile mobileNavElementContainer">
+                <img className='pawMobileNav' src={pawMobileNav} alt="" />
+                <p onClick={()=>gotoURL("/createConsultation")}>Kontultáció</p>
+            </div>
+             <div className="crucialToKnowMobile mobileNavElementContainer">
+                <img className='pawMobileNav' src={pawMobileNav} alt="" />
+                <p onClick={()=>gotoURL("/crucialToKnow")}>Fontos Tudnivalók</p>
+            </div>
+             <div className="faqMobile mobileNavElementContainer">
+                <img className='pawMobileNav' src={pawMobileNav} alt="" />
+                <p onClick={()=>gotoURL("/faq")}>Gyakran Ismételt Kérdések</p>
+            </div>
+             <div className="contact mobileNavElementContainer">
+                <img className='pawMobileNav' src={pawMobileNav} alt="" />
+                <p onClick={()=>gotoURL("/contact")}>Kapcsolat</p>
+            </div>
+             <div className="aboutme mobileNavElementContainer">
+                <img className='pawMobileNav' src={pawMobileNav} alt="" />
+                <p onClick={()=>gotoURL("/aboutmemobile")}>Rólam</p>
+            </div>
+        </div>
     </div>
    </>
   )
